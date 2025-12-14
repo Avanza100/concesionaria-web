a799742d05e8dc498ebdf497f725192feb0d3d8a
Avanza100 <marcaphilips@gmail.com>
2025-12-14 15:27:20 -0300

    Update detalle.js

--- detalle.js
+++ detalle.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "cars";
-const WHATSAPP_NUMBER = "543755541075"; // 👈 CAMBIÁ ESTE NÚMERO
+const WHATSAPP_NUMBER = "5437555307174"; // 👈 CAMBIÁ ESTE NÚMERO
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
